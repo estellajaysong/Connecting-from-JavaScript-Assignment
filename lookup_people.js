4035075a84b17028fc3eb0be1ca553a6b4a68599
Estella Song <estellajaysong@gmail.com>
2019-03-26 20:29:29 +0000

    refactored Connecting from JavaScript Assignment, where data access and input/output are now separate functions

--- lookup_people.js
+++ lookup_people.js
@@ -1,21 +1,5 @@
-const pg = require("pg");
-const settings = require("./settings"); // settings.json
 
-const client = new pg.Client({
-  user     : settings.user,
-  password : settings.password,
-  database : settings.database,
-  host     : settings.hostname,
-  port     : settings.port,
-  ssl      : settings.ssl
-});
-
-const arg = process.argv[2];
-
-client.connect((err) => {
-  if (err) {
-    return console.error("Connection Error", err);
-  }
+function lookupPeople(client, arg) {
   client.query("SELECT id, first_name, last_name, birthdate FROM famous_people WHERE first_name = $1 OR last_name = $1", [arg], (err, result) => {
     if (err) {
       return console.error("error running query", err);
@@ -24,9 +8,11 @@ client.connect((err) => {
     console.log(`Found ${result.rows.length} person(s) by the name '${arg}':`);
     let i = 1;
     result.rows.forEach((row) => {
-    console.log(`- ${i}: ${row.first_name} ${row.last_name}, born '${row.birthdate.toISOString().split('T').slice(0, 1)}'`);
-    i += 1;
+      console.log(`- ${i}: ${row.first_name} ${row.last_name}, born '${row.birthdate.toISOString().split('T').slice(0, 1)}'`);
+      i += 1;
     });
     client.end();
   });
-});
+};
+
+exports.lookupPeople = lookupPeople;
