3ff2182d33ba73e32542db0f8e1fa2310ccc439f
Estella Song <estellajaysong@gmail.com>
2019-03-27 01:52:57 +0000

    completed Create New Migration step of Database Migrations Assignment

---
+++ migrations/20190327014236_create_milestones.js_v2.js
@@ -0,0 +1,10 @@
+
+exports.up = function(knex, Promise) {
+  return knex.schema.table('milestones', function (table) {
+    table.integer('famous_person_id');
+    table.foreign('famous_person_id').references('id').inTable('famous_people');
+  });
+}
+exports.down = function(knex, Promise) {
+  return knex.schema.dropTableIfExists('milestones');
+};
